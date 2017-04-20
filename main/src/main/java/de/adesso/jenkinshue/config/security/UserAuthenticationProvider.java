755e08deaa5e4200f6c70ad3516a3020ead4b62d
fi3te <fiete.wennier@adesso.de>
2017-04-20 17:03:02 +0200

    Build error fixed

--- main/src/main/java/de/adesso/jenkinshue/config/security/UserAuthenticationProvider.java
+++ main/src/main/java/de/adesso/jenkinshue/config/security/UserAuthenticationProvider.java
@@ -1,6 +1,7 @@
 package de.adesso.jenkinshue.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.Authentication;
@@ -11,33 +12,32 @@ import de.adesso.jenkinshue.common.service.UserService;
 import lombok.extern.log4j.Log4j2;
 
 /**
- * 
  * @author wennier
- *
  */
 @Log4j2
 @Component
 public class UserAuthenticationProvider implements AuthenticationProvider {
-	
+
 	@Autowired
+	@Qualifier("userServiceImpl")
 	UserService userService;
-	
+
 	@Override
 	public boolean supports(Class<?> authentication) {
 		return true;
 	}
-	
+
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		log.debug("Prüfen, ob sich der Nutzer in der DB befindet...");
-		
-		if(userService.findByLogin(authentication.getName().toLowerCase()) == null) {
+
+		if (userService.findByLogin(authentication.getName().toLowerCase()) == null) {
 			log.debug("Der Nutzer " + authentication.getName() + " befindet sich nicht in der DB.");
 			throw new DisabledException(authentication.getName()); // muss eine AccountStatusException sein!!
 		}
-		
+
 		log.debug("Der Nutzer " + authentication.getName() + " befindet sich in der DB.");
-		
+
 		return null; // danach wird Provider gefragt (ldapAuthentication)
 	}
 
